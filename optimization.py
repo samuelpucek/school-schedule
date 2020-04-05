00c4428c31a055e4f902290726495e2fb6c029cb
Samuel Pucek <samuel.pucek@avast.com>
2020-04-05 10:58:09 +0200

    Four class schedule

--- optimization.py
+++ optimization.py
@@ -24,7 +24,7 @@ class Scheduler:
         self.C = len(limits)  # number of classes
         self.S = len(subjects)  # number of subjects
         self.D = 5  # days of week
-        self.H = 5  # hours per day
+        self.H = 6  # hours per day
 
         # Save as range
         self.C = range(self.C)
@@ -53,43 +53,51 @@ class Scheduler:
         model = Model('schedule')
 
         # Binary decision variables
-        x = [[[model.add_var(var_type=BINARY, name='({},{},{})'.format(s + 1, d + 1, h + 1))
-               for h in self.H] for d in self.D] for s in self.S]
+        x = [[[[model.add_var(var_type=BINARY,
+                              name='class:{}, subj:{}, day:{}, hour:{})'.format(c + 1, s + 1, d + 1, h + 1))
+                for h in self.H] for d in self.D] for s in self.S] for c in self.C]
 
-        # Def objective function
-        model.objective = maximize(xsum(x[s][d][h] for s in self.S for d in self.D for h in self.H))
+        # Objective function
+        model.objective = maximize(xsum(x[c][s][d][h] for c in self.C for s in self.S for d in self.D for h in self.H))
 
         # Constraints
-        for s in self.S:
-            model += xsum(x[s][d][h] for h in self.H for d in self.D) == self.lim[0][s]
+        # Satisfy initial conditions
+        for (c, s) in product(self.C, self.S):
+            model += xsum(x[c][s][d][h] for h in self.H for d in self.D) == self.lim[c][s]
 
-        for (d, h) in product(self.D, self.H):
-            model += xsum(x[s][d][h] for s in self.S) <= 1
+        # Max one lesson in one slot
+        for (c, d, h) in product(self.C, self.D, self.H):
+            model += xsum(x[c][s][d][h] for s in self.S) <= 1
 
-        for (s, d) in product(self.S, self.D):
-            model += xsum(x[s][d][h] for h in self.H) <= 2  # TODO opravit
+        # Max limit for the same subject in the same day
+        daily_limits = [[i // 5 + 1 for i in self.lim[c]] for c in self.C]
+        for (c, s, d) in product(self.C, self.S, self.D):
+            model += xsum(x[c][s][d][h] for h in self.H) <= daily_limits[c][s]
+        # TODO: Celkom OK, 5x za tyzden len limit 2 (chyba), dvojhodinovky VYV (pripadne ine predmety) dorobit
 
         # Optimization
         model.optimize()
 
-        # TODO: ako spravne handlovat premenne medzi metodami? Kukni sa do Buresa aka STB
         return x
 
     def _printer(self, x):
         """
         Transform optimal solution (binary decision variables) and create nice human readable schedules.
         """
+        cnt = 0
 
-        # Rozvrh
-        df = pd.DataFrame(index=['Pon', 'Utr', 'Str', 'Stv', 'Pia'], columns=[1, 2, 3, 4, 5])
+        # Schedules
+        for c in self.C:
 
-        for (s, d, h) in product(self.S, self.D, self.H):
-            if x[s][d][h].x == 1:
-                df.iloc[d, h] = self.sub[s]
+            list_of_hours = [h + 1 for h in self.H]
+            df = pd.DataFrame(index=['Pon', 'Utr', 'Str', 'Stv', 'Pia'], columns=list_of_hours)
+            for (s, d, h) in product(self.S, self.D, self.H):
+                if x[c][s][d][h].x == 1:
+                    df.iloc[d, h] = self.sub[s]
+                    cnt = cnt + 1
 
-        df.fillna('   ', inplace=True)
-        print(df)
-
+            df.fillna('   ', inplace=True)
+            print(df)
+            print()
+        print('cnt: {}'.format(cnt))
         return x
-
-# TODO: Prerob to na CLASS, do init daj S, H, D, dve metody: optimize, refacotr / translate
