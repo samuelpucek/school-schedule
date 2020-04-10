0ec36a5d3b50b78d12bb920f4334992c30fd6a73
Samuel Pucek <samuel.pucek@avast.com>
2020-04-10 19:59:00 +0200

    Teachers schedules implemented

--- input.py
+++ input.py
@@ -18,31 +18,74 @@ def reader():
     2       VLA   0    0     1    2
     3       MAT   4    5     5    4
 
-    :return:
+    Return
+    ------
     sub_dict: dictionary of subjects
     limits: list of hour limits
     """
 
     # Import csv as DataFrame
-    # df = pd.read_csv('resources/limits_basic.csv')
-    df = pd.read_csv('resources/limits_medium.csv')
+    # df = pd.read_csv('resources/limits_medium.csv')
+    df = pd.read_csv('resources/limits_bc.csv')
 
-    # Extract subjects
-    sub = df['subjects']
-
-    # Convert subjects into dictionary
-    sub_dict: dict = {i: sub[i] for i in range(len(sub))}
+    # Extract subjects, convert into dictionary
+    subjects = df['subjects']
+    subjects_dict_reverse: dict = {i: subjects[i] for i in range(len(subjects))}
+    subjects_dict: dict = {subjects[i]: i for i in range(len(subjects))}
 
     # Number of rows, columns
     row, col = df.shape
 
-    # Select limiting columns indexes
-    cols = [i for i in range(col) if i % 2 == 1]
-
     # Select limiting columns
-    limits = df.iloc[:, cols].T.values.tolist()
+    col_index = [i for i in range(col) if i % 2 == 1]  # Select limiting columns indexes
+    limits = df.iloc[:, col_index].T.values.tolist()
 
     # Extract classes
-    classes = [df.columns.tolist()[i] for i in cols]
+    classes_labels = [df.columns.tolist()[i] for i in col_index]
+    classes_dict_reverse: dict = {i: classes_labels[i] for i in range(len(classes_labels))}
+    classes_dict: dict = {classes_labels[i]: i for i in range(len(classes_labels))}
 
-    return sub_dict, limits, classes
+    # Extract teachers
+    col_index = [i + 1 for i in range(col) if
+                 i % 2 == 1]  # Select limiting columns indexescol_index = [i + 1 for i in col_index]
+
+    teachers_series = pd.Series()
+
+    for i in col_index:
+        teachers_series = teachers_series.append(pd.Series(df.iloc[:, i].unique()))
+
+    teachers_series = pd.Series.dropna(teachers_series)
+    teachers_series = teachers_series.unique()
+    teachers_list = teachers_series.tolist()
+
+    teachers_dict_reverse: dict = {i: teachers_list[i] for i in range(len(teachers_list))}
+    teachers_dict: dict = {teachers_list[i]: i for i in range(len(teachers_list))}
+
+    # Select (teacher, class) pairs (with respect to subject)
+    col_index = [i for i in range(col) if i % 2 == 0]  # Indexes
+    teachers_table = df.iloc[:, col_index]  # teachers table
+    teachers_table = teachers_table.fillna('nobody')  # fill NaN
+    teachers_table.columns = ['subjects'] + classes_labels  # rename columns
+
+    # Teachers restrictions connected with (subject-class) pairs
+    teachers_restrictions = []
+
+    for t in teachers_list:
+        res = []
+        for c in classes_labels:
+            lst = teachers_table[teachers_table[c] == t]['subjects']
+            res = res + [(s, c) for s in lst]
+        teachers_restrictions.append(res)
+
+    # Rewritten to dictionary coding
+    teachers_restrictions_code = []
+
+    for t in range(len(teachers_restrictions)):
+        res = []
+        for pair in teachers_restrictions[t]:
+            s, c = pair  # s, c = ('SJL', 'I.A')
+            pair_code = (subjects_dict[s], classes_dict[c])
+            res.append(pair_code)
+        teachers_restrictions_code.append(res)
+
+    return subjects_dict_reverse, limits, classes_labels, teachers_restrictions_code, teachers_dict_reverse
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import input
 import optimization
 
-subjects, limits, classes = input.reader()
+subjects, limits, classes, teachers_restrictions_code, teachers_dict_reverse = input.reader()
 
-my_schedule = optimization.Scheduler(subjects, limits, classes)
+my_schedule = optimization.Scheduler(subjects, limits, classes, teachers_restrictions_code, teachers_dict_reverse)
 my_schedule.schedule()
 
-# print(subjects)
-# print(limits)
-# print(classes)
-
+# TODO: Niektory ucitelia mozu ucit len v niektore dni
+# TODO: Pridaj dvojhodinovky (SJL SJL), (VYV, VYV) atd..
+# TODO: Predmety ktore su len napr. 2x za tyzden nech su dostatocne odseba
